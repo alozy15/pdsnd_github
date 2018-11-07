6d27c8b1c9d4b6425a9bb8e3efc4b94e6115c2f0
A.J. Lozano <alozy15@gmail.com>
2018-11-07 15:12:58 -0800

    added second language to ending message

--- Bike Share Version 3.py
+++ Bike Share Version 3.py
@@ -15,77 +15,77 @@ CITY_DATA = {'chicago':'chicago.csv',
             'washington':'washington.csv'}
 
 def city_input():
-    
-   
+
+
     print('Hello! I see you\'re interested in exploring bike share data.')
     print('\n')
-        
+
     city = input('Would you like to see data for Washington, Chicago or New York?: ')
-    
-    
-    while True:     
+
+
+    while True:
             if city == 'chicago':
                 print('The Windy City, deep dish time!')
                 print('Let\'s check out Chicago\'s bike share characteristics.')
                 print('Give it a sec...after all, patience is a virtue.')
                 return 'chicago'
-            
+
             if city.lower() == 'new york':
                 print('The Big Apple!, That\'s a great choice.')
                 print('Let\'s check out New York\'s bike share characteristics.')
                 print('Give it a sec...after all, patience is a virtue.')
                 return 'new york city'
-            
+
             if city.lower() == 'new york city':
                 print('The Big Apple!, That\'s a great choice.')
                 print('Let\'s check out New York\'s bike share characteristics.')
                 print('Give it a sec...after all, patience is a virtue.')
                 return 'new york city'
-            
+
             elif city.lower() == 'washington':
                 print('The Capital, another great choice!')
                 print('Let\'s check out Washington\'s bike share characteristics.')
                 print('Give it a sec...after all, patience is a virtue.')
                 return 'washington'
-           
+
             else:
                 print('I\'ve always wanted to visit, but I apologize, I don\'t have data for that city.')
                 city= input('please choose between Washington, Chicago or New York City.')
-                
-                
+
+
     return city
 
 def get_time():
-    
-    period = input('\n Would you like bike share characteristics broken down by month including day of the month, day of the week, or no filter at all? \n PLEASE ENTER MONTH, DAY, or NO:')
-    
 
-    while True: 
-        
+    period = input('\n Would you like bike share characteristics broken down by month including day of the month, day of the week, or no filter at all? \n PLEASE ENTER MONTH, DAY, or NO:')
+
+
+    while True:
+
         if period.lower() == 'month':
             while True:
                 day_month = input('\n Do you want the bike share data broken down by day of the month as well?:').lower()
                 if day_month == 'no':
                     print('\n Let\'s check out the monthly data,\n, give it a sec...')
                     return 'month'
-                
+
                 if day_month.lower() == 'yes':
                     print ('\n Let\'s check out the monthly data broken down by day of the month, within a month \n')
-                    print('give it a sec...')                      
+                    print('give it a sec...')
                     return 'day_of_month'
-                
+
         if period.lower() == 'day':
             print('\n Bike share data is being broken down by day of the week \n')
             print('give it a sec...')
             return 'day_of_week'
-                                          
+
         if period.lower() == 'no':
             print('\n Your selected bike share data has no filter applied')
             return "none"
-                                          
+
         period = input('\n Would you like bike share characteristics broken down by month, day of the month, day of the week, or no filter at all? Please enter month, day, or no:\n')
-        
-def month_info(mth):      
+
+def month_info(mth):
     if mth == 'month':
         month = input('\n Which month would you like to see data for: January, February, March, April, May, or June? \n')
         while month.lower() not in ['january',  'february',  'march',  'april',  'may', 'june']:
@@ -93,11 +93,11 @@ def month_info(mth):
         return month.lower()
     else:
         return 'none'
-    
-def month_day_info(df, day_mth):    
-    
+
+def month_day_info(df, day_mth):
+
     month_day = []
-    
+
     if day_mth == "day_of_month":
         month = month_info("month")
         month_day.append(month)
@@ -105,11 +105,11 @@ def month_day_info(df, day_mth):
 
         while (True):
             print('\n PLEASE NOTE SUNDAY = 1 AND SATURDAY = 7')
-            ask = ' \n Which day of the month? \n Please type your response as an integer between 1 and 7:'                 
-            ask  = ask + str(maximum_day_month) 
+            ask = ' \n Which day of the month? \n Please type your response as an integer between 1 and 7:'
+            ask  = ask + str(maximum_day_month)
             day_mth = input(ask)
 
-            try: 
+            try:
                 day_mth = int(day_mth)
                 if 1 <= day_mth <= maximum_day_month:
                     month_day.append(day_mth)
@@ -118,26 +118,26 @@ def month_day_info(df, day_mth):
                 print("Sorry,but that\'s not a numeric value")
     else:
         return 'none'
-    
-def day_info(d):   
-    
+
+def day_info(d):
+
     if d == 'day_of_week':
         day = input('\n Which day do you want data broken down for?\n Please type- M, Tu, W, Th, F, Sa, Su: \n')
         while day.lower() not in ['m', 'tu', 'w', 'th', 'f', 'sa', 'su']:
             day = input('\n I apologize, please select a choice from either,\n M, Tu, W, Th, F, Sa, Su:\n')
         return day.lower()
-    
+
     else:
         return 'none'
-    
+
 def load_data(city):
-    
+
     print('\n Working on getting the selected bike data \n')
     print('It shouldn\'t be too long \n')
-    
+
     df = pd.read_csv(CITY_DATA[city])
 
-  
+
     df['Start Time'] = pd.to_datetime(df['Start Time'])
     df['day_of_week'] = df['Start Time'].dt.weekday_name
     df['month'] = df['Start Time'].dt.month
@@ -145,20 +145,20 @@ def load_data(city):
     return df
 
 def time_filters(df, time, month, week_day, md):
-    
-    
+
+
     print('Working on getting your selected bike share characteristics. \n')
     print('It should\'t be too long \n')
-   
+
     if time == 'month':
         months = ['january', 'february', 'march',  'april',  'may', 'june']
         month = months.index(month) + 1
         df = df[df['month'] == month]
 
-    
+
     if time == 'day_of_week':
-        days = ['Monday', 'Tuesday', 
-        'Wednesday', 'Thursday', 
+        days = ['Monday', 'Tuesday',
+        'Wednesday', 'Thursday',
         'Friday', 'Saturday', 'Sunday']
         for d in days:
             if week_day.capitalize() in d:
@@ -185,7 +185,7 @@ def max_day_month(df, month):
 
 def month_frequency(df):
     # pop month
-    
+
     print('\n Question #1a:')
     print('The most popular month for bike users.')
     m = df.month.mode()[0]
@@ -194,79 +194,79 @@ def month_frequency(df):
     return popular_month
 
 def day_frequency(df):
-    
+
     print('\n Question #1b:')
     print('The most popular day of the week for bike users is.')
     return df['day_of_week'].value_counts().reset_index()['index'][0]
 
 def hour_frequency(df):
-    
+
     print('\n Question #1c')
     print('The most popular user hour for riding is?')
     df['hour'] = df['Start Time'].dt.hour
     return df.hour.mode()[0]
 
 def stations_frequency(df):
-    
+
     print('\n Question # 2a')
-    print('The most common start station is.')    
+    print('The most common start station is.')
     start_station = df['Start Station'].value_counts().reset_index()['index'][0]
     print (start_station)
-    
+
     print('\n Question # 2b')
     print('The most common end station is')
     end_station = df['End Station'].value_counts().reset_index()['index'][0]
     print(end_station)
-    
+
     return start_station, end_station
 
 def common_trip(df):
-       
+
     print('\n Question #2c')
     print('\n The most commonly traveled route combination by riders is')
     common = df[['Start Station', 'End Station']].groupby(['Start Station', 'End Station']).size().nlargest(1)
-    
+
     return common
 
 def ride_duration(df):
-    
+
     print('\n Question #3a & #3b')
     print('The total days of use for riders, and the average trip duration per user.')
-    
+
     df['End Time'] = pd.to_datetime(df['End Time'])
     df['Travel Time'] = df['End Time'] - df['Start Time']
-    
+
     total_ride_time = np.sum(df['Travel Time'])
     total_days = str(total_ride_time).split()[0]
 
     print ('\n Riders amounted', total_days, ' days of total use. \n')
-    
+
     mean_ride_time = np.mean(df['Travel Time'])
     mean_time = str(mean_ride_time).split()[0]
-    
+
     print('Riders on average use bike services about', mean_time, 'days. \n')
 
     return total_ride_time, mean_time
 
 def bike_users(df):
-    
+
     print('\n Question #4a')
     print('\n The breakdown of user type per city is as follows: \n')
     return df['User Type'].value_counts()
 
 def gender_data(df):
-   
+
     try:
         print('\n Question #4b')
         print('\n  The gender demographics for riders in this city are: \n')
         return df['Gender'].value_counts()
-    
+
     except:
         print('Please note that Washington does not have Gender specific info.')
         print('There is no gender data in the source.')
-        
+
 def birth_years(df):
-    
+
     try:
         print('\n Question #4c')
         print('\n Birthday characteristics for users in this city are as follows: \n')
@@ -277,10 +277,10 @@ def birth_years(df):
         most_common_bday= df['Birth Year'].mode()[0]
         print ('The most frequent year of birth year of riders is', str(most_common_bday), '\n')
         return earliest, latest, most_common_bday
-    
+
     except:
         print('No available birth date data for this period.')
-        
+
 def process(f, df):
     '''Calculates the time it takes to commpute a statistic
     '''
@@ -288,25 +288,25 @@ def process(f, df):
     statToCompute = f(df)
     print(statToCompute)
     print("This stat took %s seconds." % (time.time() - start_time),'to retrieve')
-    
+
 def raw_data(df):
-   
+
     row_index = 0
 
     see_data = input('\n Would you like to see five rows of the data used to compute the stats? Please write yes or no:\n')
-    
+
     while True:
         if see_data.lower() == 'no':
             return
         if see_data.lower() == 'yes':
             print(df[row_index: row_index + 5])
             row_index = row_index + 5
-            
+
         see_data = input('\n Would you like to see five more rows of the data used to compute the stats?\n  Please write yes or no: \n')
-        
+
 def main():
-   
-    
+
+
      #calling all the functions step by step
     city = city_input()
     df = load_data(city)
@@ -317,24 +317,24 @@ def main():
 
     df = time_filters(df, period, month, day, month_day)
     raw_data(df)
-    
+
 
     stats_list = [month_frequency,
-     day_frequency, hour_frequency, 
-     ride_duration, common_trip, 
+     day_frequency, hour_frequency,
+     ride_duration, common_trip,
      stations_frequency, bike_users, birth_years, gender_data]
 
     for stat in stats_list:
         process(stat, df)
 
-    
+
     restart = input("\n * Would you like to do it again and perform another analysis? Type \'yes\' or \'no\'.\n")
     if restart.lower() == 'yes' or restart.lower() == 'y':
         main()
-        
+
     if restart.lower() == 'no':
-        print('Adios Amigo \n Thanks for stopping by.')
-        
+        print('Adios Amigo, Goodbye Friend \n Thanks for stopping by.')
+
     exit()
-    
+
 main()
